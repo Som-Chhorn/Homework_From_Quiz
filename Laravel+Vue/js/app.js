54f41959e4b4a8f6996b5ba5c365b1e23c860a17
SOM_CHHORN <som.chhorn@student.passerellesnumeriques.org>
2021-11-10 21:36:20 +0700

    Homework

---
+++ Laravel+Vue/js/app.js
@@ -0,0 +1,73 @@
+new Vue({
+  el: "#app",
+  data: {
+    todos: [],
+    authors: [],
+    author_id: null,
+    title: null,
+    body: null,
+    updateId: null,
+    isHidden: false,
+  },
+  methods: {
+    getInfor() {
+      let base_url = "http://127.0.0.1:8000/api/books";
+      axios.get(base_url).then((res) => {
+        console.log(res.data);
+        this.todos = res.data;
+      });
+    },
+    createTodo() {
+      let base_url = "http://127.0.0.1:8000/api/books";
+      let data = {
+        author_id: parseInt(this.author_id),
+        title: this.title,
+        body: this.body,
+      };
+      axios
+        .post(base_url, data)
+        .then((res) => {
+          this.getInfor();
+        })
+        .catch(() => {
+          console.log("Error 404 not found!");
+        });
+      this.author_id = "";
+      this.title = "";
+      this.body = "";
+    },
+    deleteTodo(todos) {
+      let id = todos.id;
+      let url = "http://127.0.0.1:8000/api/books";
+      axios.delete(url + id).then((response) => {
+        this.todos = response.data;
+        console.log("Deleted");
+      });
+    },
+    //Edit TODO
+    editTodo(todos) {
+      this.author_id = todos.author_id;
+      this.title = todos.title;
+      this.body = todos.body;
+      this.updateId = todos.id;
+      this.isHidden = true;
+    },
+    // Update TODO
+    updateTodo() {
+      let data = {
+        author_id: parseInt(this.author_id),
+        title: this.title,
+        body: this.body,
+      };
+      axios
+        .put("http://127.0.0.1:8000/api/books" + parseInt(this.updateId), data)
+        .then(() => {
+          window.location.reload();
+        });
+      this.isHidden = false;
+    },
+  },
+  mounted() {
+    this.getInfor();
+  },
+});
